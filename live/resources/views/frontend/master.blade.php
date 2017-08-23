4eb7ef3f84b9de690832af44b1985a7604693dec
rajeshkcbars <rajeshkcbars@gmail.com>
2017-08-23 13:09:05 +0545

    file updated with insitution

---
+++ live/resources/views/frontend/master.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Europe Study Center</title>
+    <meta name="description" content="">
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+    <!--Stylesheet
+    ================================================== -->
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/screen.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/slicknav.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/animated.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/select2.min.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/lib/css/isotope.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/live/public/css/style.css">
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Google Webfonts
+    ================================================== -->
+    <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+
+    <script src="{{URL::to('/')}}/live/public/lib/js/vendor/modernizr-2.7.1.min.js"></script>
+</head>
+@yield('content')
+<footer id="footer">
+    <div class="top-footer">
+        <div class="container">
+            <ul class="footer-menus">
+                <li class="col-sm-4 col-xs-6">
+        <span class="menu-title">
+          Insitution
+        </span>
+                    <a href="#">
+                        Australia
+                    </a>
+                    <a href="#">
+                        Canada
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                </li>
+                <li class="col-sm-4 col-xs-6">
+        <span class="menu-title">
+          Consultants
+        </span>
+                    <a href="#">
+                        Australia
+                    </a>
+                    <a href="#">
+                        Canada
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                </li>
+                <li class="col-sm-4 col-xs-6">
+        <span class="menu-title">
+          Students
+        </span>
+                    <a href="#">
+                        Australia
+                    </a>
+                    <a href="#">
+                        Canada
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                    <a href="#">
+                        America
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="bottom-footer">
+        <div class="container">
+            <a class="credit" href="#" target="_blank" style=" display:inline-block;">Website Designed and Developed By:
+                <img src="{{URL::to('/')}}/live/public/images/logo.png"></a>
+        </div>
+    </div>
+
+</footer>
+
+<script src="{{URL::to('/')}}/live/public/lib/js/vendor/jquery-1.11.0.min.js"></script>
+<script src="{{URL::to('/')}}/live/public/lib/js/vendor/bootstrap.min.js"></script>
+<script src="{{URL::to('/')}}/live/public/lib/js/select2.min.js"></script>
+<script src="{{URL::to('/')}}/live/public/lib/js/jquery.isotope.js"></script>
+<script src="{{URL::to('/')}}/live/public/lib/js/plugins.js"></script>
+<script src="{{URL::to('/')}}/live/public/lib/js/scripts.js"></script>
+<!--[if lt IE 9]>
+<script src="lib/js/vendor/selectivizr-min.js"></script>
+<![endif]-->
+</body>
+</html>
